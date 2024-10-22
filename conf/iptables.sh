81065b1e0a33898d6ef1cfe7f3f2392a58bf0002
Félix Piédallu <felix@piedallu.me>
2024-10-22 15:07:57 +0200

    customize iface

--- conf/iptables.sh
+++ conf/iptables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-iptables -t nat -A PREROUTING -i ens19 -p tcp --dport 53 -j REDIRECT --to-port 1053
-iptables -t nat -A PREROUTING -i ens19 -p udp --dport 53 -j REDIRECT --to-port 1053
+iptables -t nat -A PREROUTING -i __IFACE__ -p tcp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
+iptables -t nat -A PREROUTING -i __IFACE__ -p udp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
 
-ip6tables -t nat -A PREROUTING -i ens19 -p tcp --dport 53 -j REDIRECT --to-port 1053
-ip6tables -t nat -A PREROUTING -i ens19 -p udp --dport 53 -j REDIRECT --to-port 1053
+ip6tables -t nat -A PREROUTING -i __IFACE__ -p tcp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
+ip6tables -t nat -A PREROUTING -i __IFACE__ -p udp --dport 53 -j REDIRECT --to-port __PORT_NAMED__
